f16f8d6929c90fa2dd674ea0fa7f41cc31cb61e5
DevExpressExampleBot <fakeEmail>
2023-03-16 14:27:09 +0400

    Source auto update [skip ci]

--- VB/WpfApplication68/MainWindow.xaml.vb
+++ VB/WpfApplication68/MainWindow.xaml.vb
@@ -1,159 +1,54 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 
 Namespace WpfApplication68
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Public Module Helper
-		Public ReadOnly Property Data() As List(Of Employee)
-			Get
-				Dim stuff As New List(Of Employee)()
-				stuff.Add(New Employee() With {
-					.ID = 1,
-					.ParentID = 0,
-					.Name = "Gregory S. Price",
-					.Department = "",
-					.Position = "President"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 2,
-					.ParentID = 1,
-					.Name = "Irma R. Marshall",
-					.Department = "Marketing",
-					.Position = "Vice President"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 3,
-					.ParentID = 1,
-					.Name = "John C. Powell",
-					.Department = "Operations",
-					.Position = "Vice President"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 4,
-					.ParentID = 1,
-					.Name = "Christian P. Laclair",
-					.Department = "Production",
-					.Position = "Vice President"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 5,
-					.ParentID = 1,
-					.Name = "Karen J. Kelly",
-					.Department = "Finance",
-					.Position = "Vice President"
-				})
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-				stuff.Add(New Employee() With {
-					.ID = 6,
-					.ParentID = 2,
-					.Name = "Brian C. Cowling",
-					.Department = "Marketing",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 7,
-					.ParentID = 2,
-					.Name = "Thomas C. Dawson",
-					.Department = "Marketing",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 8,
-					.ParentID = 2,
-					.Name = "Angel M. Wilson",
-					.Department = "Marketing",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 9,
-					.ParentID = 2,
-					.Name = "Bryan R. Henderson",
-					.Department = "Marketing",
-					.Position = "Manager"
-				})
+    Public Module Helper
 
-				stuff.Add(New Employee() With {
-					.ID = 10,
-					.ParentID = 3,
-					.Name = "Harold S. Brandes",
-					.Department = "Operations",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 11,
-					.ParentID = 3,
-					.Name = "Michael S. Blevins",
-					.Department = "Operations",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 12,
-					.ParentID = 3,
-					.Name = "Jan K. Sisk",
-					.Department = "Operations",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 13,
-					.ParentID = 3,
-					.Name = "Sidney L. Holder",
-					.Department = "Operations",
-					.Position = "Manager"
-				})
+        Public ReadOnly Property Data As List(Of Employee)
+            Get
+                Dim stuff As List(Of Employee) = New List(Of Employee)()
+                stuff.Add(New Employee() With {.ID = 1, .ParentID = 0, .Name = "Gregory S. Price", .Department = "", .Position = "President"})
+                stuff.Add(New Employee() With {.ID = 2, .ParentID = 1, .Name = "Irma R. Marshall", .Department = "Marketing", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 3, .ParentID = 1, .Name = "John C. Powell", .Department = "Operations", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 4, .ParentID = 1, .Name = "Christian P. Laclair", .Department = "Production", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 5, .ParentID = 1, .Name = "Karen J. Kelly", .Department = "Finance", .Position = "Vice President"})
+                stuff.Add(New Employee() With {.ID = 6, .ParentID = 2, .Name = "Brian C. Cowling", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 7, .ParentID = 2, .Name = "Thomas C. Dawson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 8, .ParentID = 2, .Name = "Angel M. Wilson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 9, .ParentID = 2, .Name = "Bryan R. Henderson", .Department = "Marketing", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 10, .ParentID = 3, .Name = "Harold S. Brandes", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 11, .ParentID = 3, .Name = "Michael S. Blevins", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 12, .ParentID = 3, .Name = "Jan K. Sisk", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 13, .ParentID = 3, .Name = "Sidney L. Holder", .Department = "Operations", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 14, .ParentID = 4, .Name = "James L. Kelsey", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 15, .ParentID = 4, .Name = "Howard M. Carpenter", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 16, .ParentID = 4, .Name = "Jennifer T. Tapia", .Department = "Production", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 17, .ParentID = 5, .Name = "Judith P. Underhill", .Department = "Finance", .Position = "Manager"})
+                stuff.Add(New Employee() With {.ID = 18, .ParentID = 5, .Name = "Russell E. Belton", .Department = "Finance", .Position = "Manager"})
+                Return stuff
+            End Get
+        End Property
+    End Module
 
-				stuff.Add(New Employee() With {
-					.ID = 14,
-					.ParentID = 4,
-					.Name = "James L. Kelsey",
-					.Department = "Production",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 15,
-					.ParentID = 4,
-					.Name = "Howard M. Carpenter",
-					.Department = "Production",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 16,
-					.ParentID = 4,
-					.Name = "Jennifer T. Tapia",
-					.Department = "Production",
-					.Position = "Manager"
-				})
+    Public Class Employee
 
-				stuff.Add(New Employee() With {
-					.ID = 17,
-					.ParentID = 5,
-					.Name = "Judith P. Underhill",
-					.Department = "Finance",
-					.Position = "Manager"
-				})
-				stuff.Add(New Employee() With {
-					.ID = 18,
-					.ParentID = 5,
-					.Name = "Russell E. Belton",
-					.Department = "Finance",
-					.Position = "Manager"
-				})
-				Return stuff
-			End Get
-		End Property
-	End Module
+        Public Property ID As Integer
 
-	Public Class Employee
-		Public Property ID() As Integer
-		Public Property ParentID() As Integer
-		Public Property Name() As String
-		Public Property Position() As String
-		Public Property Department() As String
-	End Class
+        Public Property ParentID As Integer
+
+        Public Property Name As String
+
+        Public Property Position As String
+
+        Public Property Department As String
+    End Class
 End Namespace
